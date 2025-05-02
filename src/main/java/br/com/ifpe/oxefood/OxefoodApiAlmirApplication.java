737f70d3bf62db77fc3172b2feae0d0dea331cc9
Almir Rogerio <ars32@discente.ifpe.edu.br>
2025-05-02 09:49:09 -0300

    primeiro envio config do pprojeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiAlmirApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiAlmirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiAlmirApplication.class, args);
+	}
+
+}
